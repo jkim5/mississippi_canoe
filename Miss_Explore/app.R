9c92967f1d91f554ee871e84d6af8db35771a471
Ellen Graham <grahamammal@gmail.com>
2020-01-18 18:19:05 -0600

    Revert "Attempted to make map more detailed"
    This reverts commit 0c1b8ff1632787d72e137a6bf4b89b484aa080cd.

--- Miss_Explore/app.R
+++ Miss_Explore/app.R
@@ -8,63 +8,21 @@ library(viridis)
 library(dplyr)
 library(glue)
 library(htmltools)
-library(purrr)
-
-miss_basin_simple <- st_read("clean_data/miss_basin_simple/Miss_RiverBasin.shp")
 
 
+(list.files(recursive = TRUE))
 
-clean_legs <- st_read("clean_data/clean_legs/clean_data_geom.shp")
+miss_basin <- st_read("clean_data/miss_basin/Miss_RiverBasin.shp") %>% 
+  st_transform(crs = 4326)
+miss_basin_simple <- rmapshaper::ms_simplify(miss_basin)
+
+
+
+all_legs_med <- read_csv("clean_data/clean_data_non_geom.csv")
+clean_legs <- st_as_sf(all_legs_med, coords = c("longitude", "latitude"), crs = 4326)
 tiny_legs = clean_legs[seq(1, nrow(clean_legs), 50), ]
 
-section_1_poly <- st_polygon(x = list(rbind(c(-95.66165, 47.00948),
-                                  c(-95.66165, 47.7275),
-                                  c(-94.68412, 47.7275), 
-                                  c(-94.68412, 47.00948),
-                                  c(-95.66165, 47.00948))))
 
-section_2_poly <- st_polygon(x = list(rbind(c(-90.70569, 43.13306),
-                                  c(-90.70569, 43.89789),
-                                  c(-91.73814, 43.89789), 
-                                  c(-91.73814, 43.13306),
-                                  c(-90.70569, 43.13306))))
-
-section_3_poly <- st_polygon(x = list(rbind(c(-88.49971, 35.90685),
-                                  c(-88.49971, 39.43619),
-                                  c(-91.04787, 39.43619), 
-                                  c(-91.04787, 35.90685),
-                                  c(-88.49971, 35.90685))))
-
-section_4_poly <- st_polygon(x = list(rbind(c(-89.12831, 29.17231),
-                                  c(-89.12831, 30.58212),
-                                  c(-91.37375, 30.58212), 
-                                  c(-91.37375, 29.17231),
-                                  c(-89.12831, 29.17231))))
-
-
-section_data <- map(1:4, ~filter(clean_legs, clean_legs$section == .x))
-section_1_data <- section_data[[1]]
-
-map_section_k <- function(k, cont_var) {
-  my_pal <-  colorNumeric("plasma", clean_legs[[cont_var]])
-  
-  leafletProxy("map", data = section_data[[k]]) %>%
-          clearMarkers() %>%
-          addCircleMarkers(radius = 4,
-                           color = my_pal(section_data[[k]][[cont_var]]),
-                           fill = TRUE,
-                           fillColor = my_pal(section_data[[k]][[cont_var]]),
-                           popup = glue('{section_data[[k]]$date_time}<br> Value: {tiny_legs[[cont_var]]}'),
-                           label = glue('Value: {section_data[[k]][[cont_var]]}'),
-                           group = "sparse_markers"
-          ) %>%
-          clearControls() %>%
-          addLegend(position = "bottomright",
-            pal = my_pal,
-            values = clean_legs[[cont_var]],
-            title = cont_var
-          )
-}
 
 
 ui <- bootstrapPage(
@@ -78,10 +36,7 @@ ui <- bootstrapPage(
 )
 
 server <- function(input, output, session) {
-  current_shown_section <- -1
-  force_update = TRUE
-  
-  
+    
   my_pal <- reactive({
       colorNumeric("plasma", tiny_legs[[input$cont_var]])
   })
@@ -94,106 +49,26 @@ server <- function(input, output, session) {
         addPolygons(fillOpacity = 0, opacity = 0.5)
   })
 
-  # observe({
-  #   leafletProxy("map", data = tiny_legs) %>%
-  #     clearMarkers() %>%
-  #     addCircleMarkers(radius = 4,
-  #                      color = my_pal()(tiny_legs[[input$cont_var]]),
-  #                      fill = TRUE,
-  #                      fillColor = my_pal()(tiny_legs[[input$cont_var]]),
-  #                      popup = glue('{tiny_legs$date_time}<br> Value: {tiny_legs[[input$cont_var]]}'),
-  #                      label = glue('Value: {tiny_legs[[input$cont_var]]}'),
-  #                      group = "sparse_markers"
-  #     ) %>%
-  #     clearControls() %>%
-  #     addLegend(position = "bottomright",
-  #       pal = my_pal(),
-  #       values = ~(eval(parse(text = input$cont_var))),
-  #       title = input$cont_var
-  #     )
-  # })
-
   observe({
-    input$cont_var
-    force_update <<- TRUE
+    leafletProxy("map", data = tiny_legs) %>% 
+      clearMarkers() %>% 
+      addCircleMarkers(radius = 4, 
+                       color = my_pal()(tiny_legs[[input$cont_var]]),
+                       fill = TRUE,
+                       fillColor = my_pal()(tiny_legs[[input$cont_var]]),
+                       popup = glue('{tiny_legs$date_time}<br> Value: {tiny_legs[[input$cont_var]]}'),
+                       label = glue('Value: {tiny_legs[[input$cont_var]]}'),
+                       group = "sparse_markers"
+      ) %>% 
+      clearControls() %>% 
+      addLegend(position = "bottomright",
+        pal = my_pal(), 
+        values = ~(eval(parse(text = input$cont_var))),
+        title = input$cont_var
+      )
   })
   
-  observeEvent({input$map_bounds},{
-    map_bounds <- input$map_bounds
-    cont_var <- input$cont_var
-    
-    if(!is.null(map_bounds)){
-      map_bounds_poly <- st_polygon(x = list(rbind(c(map_bounds$east, map_bounds$north),
-                                    c(map_bounds$east, map_bounds$south),
-                                    c(map_bounds$west, map_bounds$south), 
-                                    c(map_bounds$west, map_bounds$north),
-                                    c(map_bounds$east, map_bounds$north))))
-      shown_sections <- as.logical(st_intersects(map_bounds_poly,
-                                  st_as_sfc(list(section_1_poly,
-                                            section_2_poly, 
-                                            section_3_poly,
-                                            section_4_poly)),
-                                  sparse = FALSE))
 
-      print(current_shown_section)
-      
-      if(shown_sections[1] &&
-         sum(shown_sections) == 1 &&
-         (current_shown_section != 1||
-          force_update)) {
-        
-        map_section_k(k = 1, cont_var = cont_var)
-        current_shown_section <<- 1
-
-      } else if (shown_sections[2] &&
-         sum(shown_sections) == 1 &&
-         (current_shown_section != 2||
-          force_update)) {
-        
-        map_section_k(k = 2, cont_var = cont_var)
-        current_shown_section <<- 2
-        
-      } else if (shown_sections[3] &&
-         sum(shown_sections) == 1 &&
-         (current_shown_section != 3||
-          force_update)) {
-        
-        map_section_k(k = 3, cont_var = cont_var)
-        current_shown_section <<- 3
-        
-      } else if (shown_sections[4] &&
-         sum(shown_sections) == 1 &&
-         (current_shown_section != 4 ||
-          force_update)) {
-        
-        map_section_k(k = 4, cont_var = cont_var)
-        current_shown_section <<- 4
-        
-      } else if (sum(shown_sections) > 1 &&
-                 (current_shown_section!= 0 ||
-                  force_update)) {
-        leafletProxy("map", data = tiny_legs) %>%
-          clearMarkers() %>%
-          addCircleMarkers(radius = 4,
-                           color = my_pal()(tiny_legs[[input$cont_var]]),
-                           fill = TRUE,
-                           fillColor = my_pal()(tiny_legs[[input$cont_var]]),
-                           popup = glue('{tiny_legs$date_time}<br> Value: {tiny_legs[[input$cont_var]]}'),
-                           label = glue('Value: {tiny_legs[[input$cont_var]]}'),
-                           group = "sparse_markers"
-          ) %>%
-          clearControls() %>%
-          addLegend(position = "bottomright",
-            pal = my_pal(),
-            values = ~(eval(parse(text = input$cont_var))),
-            title = input$cont_var
-          )
-        current_shown_section <<- 0
-      }
-      
-      force_update <<- FALSE
-    }
-  })
 }
 
 shinyApp(ui, server)
--- Miss_Explore/data_cleaning.R
+++ Miss_Explore/data_cleaning.R
@@ -132,14 +132,10 @@ all_legs <- bind_rows(leg_0_data, leg_2_data) %>%
 # manipulating data
 ##########################################
 
-# removing NAs that make anaylsis impossible
-
 all_legs_small <- all_legs %>% 
   filter(!is.na(date)) %>% 
   arrange(leg,date_time) %>% 
   filter(!is.na(air_pressure))
-
-# taking median value to smooth extreme/absurd values
 
 all_legs_median <- all_legs_small %>% 
   mutate(air_temp = ifelse(air_temp == 0, NA, air_temp),
@@ -152,41 +148,5 @@ all_legs_median <- all_legs_small %>%
   summarise_at(6:23, median, na.rm = TRUE) %>% 
   mutate(date_time = as_datetime(fifteen_sec_chunk)) %>% 
   mutate(index = 1:n())
-
-# partitioning data into 4 distinct sections to ease mapping
-
-section_1_poly <- st_polygon(x = list(rbind(c(-95.66165, 47.00948),
-                                  c(-95.66165, 47.7275),
-                                  c(-94.68412, 47.7275), 
-                                  c(-94.68412, 47.00948),
-                                  c(-95.66165, 47.00948))))
-
-section_2_poly <- st_polygon(x = list(rbind(c(-90.70569, 43.13306),
-                                  c(-90.70569, 43.89789),
-                                  c(-91.73814, 43.89789), 
-                                  c(-91.73814, 43.13306),
-                                  c(-90.70569, 43.13306))))
-
-section_3_poly <- st_polygon(x = list(rbind(c(-88.49971, 35.90685),
-                                  c(-88.49971, 39.43619),
-                                  c(-91.04787, 39.43619), 
-                                  c(-91.04787, 35.90685),
-                                  c(-88.49971, 35.90685))))
-
-section_4_poly <- st_polygon(x = list(rbind(c(-89.12831, 29.17231),
-                                  c(-89.12831, 30.58212),
-                                  c(-91.37375, 30.58212), 
-                                  c(-91.37375, 29.17231),
-                                  c(-89.12831, 29.17231))))
-
-st_as_sf(all_legs_med, coords = c("longitude", "latitude"), crs = 4326) %>% 
-  mutate(section = ifelse(as.logical(st_within(geometry, section_1_poly, sparse = FALSE)), 1, 0),
-         section = ifelse(as.logical(st_within(geometry, section_2_poly, sparse = FALSE)), 2, section),
-         section = ifelse(as.logical(st_within(geometry, section_3_poly, sparse = FALSE)), 3, section),
-         section = ifelse(as.logical(st_within(geometry, section_4_poly, sparse = FALSE)), 4, section)) %>% 
-  st_write("clean_data/clean_legs/clean_data_geom.shp")
-
-miss_basin <- st_read("clean_data/miss_basin/Miss_RiverBasin.shp") %>% 
-  st_transform(crs = 4326) %>% 
-  rmapshaper::ms_simplify() %>% 
-  st_write("clean_data/miss_basin_simple/Miss_RiverBasin.shp")
+ 
+readr::write_csv(all_legs_median, "clean_data\\clean_data_non_geom.csv")
