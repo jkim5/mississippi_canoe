ed1cfa66b7d623851d2336ea6d82a3ab7e1bf9b9
Ellen Graham <grahamammal@gmail.com>
2020-02-04 16:23:40 -0600

    made visuals cleaner

--- Miss_Explore/app.R
+++ Miss_Explore/app.R
@@ -18,16 +18,32 @@ clean_legs <- st_read("clean_data/clean_legs/clean_data_geom.shp") %>%
 
 tiny_legs = clean_legs[seq(1, nrow(clean_legs), 10), ]
 
+units_table = tibble(name = c("alt", "speed", "heading", "climb", "accel_x", "accel_y", "accel_z", "gyro_x"
+, "gyro_y", "gyro_z", "air_tmp", "air_gas", "air_hmd", "ar_prss", "wtr_tmp"),
+                     unit = c("m", "m/s", "degrees", "m/s", "m/s^2", "m/s^2", "m/s^2", "degrees/s", "degrees/s", "degrees/s", "C", "Ohm", "%", "hPa", "Celcius"))
 
+variable_list <-list(Altitude = "alt",
+                     Speed = "speed",
+                     Heading = "heading",
+                     Climb = "climb",
+                     `X Acceleration` = "accel_x",
+                     `Y Acceleration` = "accel_y",
+                     `Z Acceleration` = "accel_z",
+                     `X Gyroscope` = "gyro_x",
+                     `Y Gyroscope` = "gyro_y",
+                     `Z Gyroscope` = "gyro_z",
+                     `Air Temperature` = "air_tmp",
+                     `Air Gas` = "air_gas",
+                     `Air Humidity` = "air_hmd",
+                     `Air Pressure` = "ar_prss",
+                     `Water Temperature` = "wtr_tmp")
 
 
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
-    selectInput("cont_var", "Select Variable",
-        colnames(select_if(tiny_legs, is.numeric))
-    )
+    selectInput("cont_var", "Select Variable",variable_list)
   )
 )
 
@@ -85,25 +101,26 @@ server <- function(input, output, session) {
     # entire map is being torn down and recreated).
     leaflet(miss_basin_simple) %>%
         addProviderTiles(providers$OpenStreetMap.HOT) %>% 
-        addPolygons(fillOpacity = 0, opacity = 0.5)
+        addPolygons(fillOpacity = 0, opacity = 0.3)
   })
 
   observe({
+    unit = units_table$unit[which(units_table$name == input$cont_var)]
     leafletProxy("map", data = tiny_legs) %>% 
       clearMarkers() %>% 
       addCircleMarkers(radius = 4, 
                        color = my_pal()(tiny_legs[[input$cont_var]]),
                        fill = TRUE,
                        fillColor = my_pal()(tiny_legs[[input$cont_var]]),
-                       popup = glue('{tiny_legs$date_time}<br> Value: {tiny_legs[[input$cont_var]]}'),
-                       label = glue('Value: {tiny_legs[[input$cont_var]]}'),
+                       popup = glue('{tiny_legs$date_time}<br> {round(tiny_legs[[input$cont_var]], 2)} {unit}'),
+                       label = glue('{round(tiny_legs[[input$cont_var]], 2)} {unit}'),
                        group = "sparse_markers"
       ) %>% 
       clearControls() %>% 
       addLegend(position = "bottomright",
         pal = my_pal(), 
         values = ~(eval(parse(text = input$cont_var))),
-        title = input$cont_var
+        title = glue("{names(variable_list)[grep(input$cont_var, variable_list)]} ({unit})")
       )
   })
   
